db3d2798b304fdb26f6cae87a1ae4e5013d04a30
Fetaksa <rustykami@yandex.ru>
2022-10-19 19:40:59 +0300

    Task09 Method added

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,12 +5,33 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
 
-int firstDigit = number / 10;  //здесь целочисленное деление, остаток отбрасывается
-int secondDigit = number % 10; //здесь получаем остаток от деления на 10
+// int firstDigit = number / 10;  //здесь целочисленное деление, остаток отбрасывается
+// int secondDigit = number % 10; //здесь получаем остаток от деления на 10
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа равна {firstDigit}");
-else Console.WriteLine ($"Наибольшая цифра числа равна {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа равна {firstDigit}");
+// else Console.WriteLine ($"Наибольшая цифра числа равна {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine ($"Наибольшая цифра числа равна {maxDigit}");
+//Тернарный оператор. Если firstDigit > secondDigit тогда(?) записываем в переменную
+//maxDigit  firstDigit. Иначе (:) записываем в переменную maxDigit secondDigit
+
+//ещё вариант тернарного оператора
+// Console.Write($"Наибольшая цифра числа {number} равна ");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
 
 //встроенный метод Math.
-int result = Math.Max(firstDigit, secondDigit);
-Console.WriteLine($"Результат = {result}");
+// int result = Math.Max(firstDigit, secondDigit);
+// Console.WriteLine($"Результат = {result}");
+
+
+//пишем метод
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа равна {maxDigit}");
