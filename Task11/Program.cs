799a77fb1c18a9cdd48436a95ae5d43037bf6015
Fetaksa <rustykami@yandex.ru>
2022-10-20 14:16:07 +0300

    Task 11 added

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -3,9 +3,24 @@
 
 // int firstDigit = number / 10;  //здесь целочисленное деление, остаток отбрасывается
 // int secondDigit = number % 10; //здесь получаем остаток от деления на 10
-
+// Console.Write($"Полученное число: {firstDigit}{thirdDigit}");
 
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное трехзначное число => {number}");
 
-int firstDigit = 
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int newDigit = firstDigit * 10 + thirdDigit;
+
+
+
+int DelSecondDigit(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    
+    return firstDigit*10 + thirdDigit;
+}
+
+int newDigit = DelSecondDigit(number);
+Console.Write($"Полученное число: {newDigit}");
