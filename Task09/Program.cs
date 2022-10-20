d37560fe25e28df56abc3bb67e8f2e6a22bc960b
Fetaksa <rustykami@yandex.ru>
2022-10-20 13:10:45 +0300

    gitignore added

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -26,10 +26,10 @@ Console.WriteLine($"Случайное число из отрезка 10-99 => {
 
 
 //пишем метод
-int MaxDigit(int number)
+int MaxDigit(int num)
 {
-    int firstDigit = number / 10;
-    int secondDigit = number % 10;
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
     return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
 
